f4d6bfe5a1a007f3c4f597e0bfc96801dac8b6fb
MrShifu01 <stander.christian.com>
2023-06-01 08:24:11 +0200

    Have the music search and store to favourites working great

--- router.js
+++ router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 async function getResults(term) {
   console.log(term)
   try {
-    const response = await axios.get(`https://itunes.apple.com/search?term=${term}&limit=5`);
+    const response = await axios.get(`https://itunes.apple.com/search?term=${term}&limit=5&media=music`);
     const results = response.data;
     return results;
   } catch (error) {
