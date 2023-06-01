645e2109ebd8a3fcb1991ef8f0a7ddd0128fc3ea
MrShifu01 <stander.christian.com>
2023-06-01 06:59:38 +0200

    new repo, first commit

---
+++ router.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import axios from 'axios';
+
+const router = express.Router();
+
+async function getResults(term) {
+  console.log(term)
+  try {
+    const response = await axios.get(`https://itunes.apple.com/search?term=${term}&limit=5`);
+    const results = response.data;
+    return results;
+  } catch (error) {
+    console.error("Error retrieving results:", error);
+    throw error;
+  }
+}
+
+// Handle the HTTP GET request to get all results
+router.get('/', async (req, res) => {
+  const term = req.query.term
+  try {
+    const results = await getResults(term);
+    res.send(results);
+  } catch (error) {
+    res.status(500).send("Error retrieving results");
+  }
+});
+
+export default router;
